5032755cee267dfb71d34c286c9a87315b220229
Savvas-Tsarouchas <savvas.tsarouchas@netcompany.com>
2026-06-19 17:28:47 +0300

    EICSRM-111: EICSRM-111: Investor Onboarding.

--- lib/civicrm-ext/nc_config/managed/050_CaseTypes.mgd.php
+++ lib/civicrm-ext/nc_config/managed/050_CaseTypes.mgd.php
@@ -3,193 +3,207 @@
 use CRM_NcConfig_ExtensionUtil as E;
 
 return [
-    [
-        'name' => 'CaseType_eic_investor_onboarding',
-        'entity' => 'CaseType',
-        'cleanup' => 'unused',
-        'update' => 'unmodified',
-        'params' => [
-            'version' => 4,
-            'values' => [
-                'name' => 'eic_investor_onboarding',
-                'title' => E::ts('Investor Onboarding'),
-                'description' => E::ts('Investor Onboarding'),
-                'definition' => [
-                    'restrictActivityAsgmtToCmsUser' => 1,
-                    'activityAsgmtGrps' => [],
-                    'activityTypes' => [
-                        [
-                            'name' => 'Open Case',
-                            'max_instances' => '1',
-                        ],
-                        [
-                            'name' => 'Email',
-                        ],
-                        [
-                            'name' => 'Follow up',
-                        ],
-                        [
-                            'name' => 'Meeting',
-                        ],
-                        [
-                            'name' => 'Phone Call',
-                        ],
-                    ],
-                    'activitySets' => [
-                        [
-                            'name' => 'standard_timeline',
-                            'label' => E::ts('Outreach'),
-                            'timeline' => 1,
-                            'activityTypes' => [
-                                [
-                                    'name' => 'Outreach response',
-                                    'label' => E::ts('Outreach response'),
-                                    'status' => 'Scheduled',
-                                    'reference_activity' => [],
-                                    'reference_offset' => '14',
-                                    'reference_select' => 'newest',
-                                    'default_assignee_type' => '1',
-                                ],
-                            ],
-                        ],
-                        [
-                            'name' => 'timeline_1',
-                            'label' => E::ts('Onboarding'),
-                            'timeline' => 1,
-                            'activityTypes' => [
-                                [
-                                    'name' => 'Onboarding call',
-                                    'label' => E::ts('Onboarding call'),
-                                    'status' => 'Scheduled',
-                                    'reference_activity' => 'Outreach response',
-                                    'reference_offset' => '1',
-                                    'reference_select' => 'newest',
-                                    'default_assignee_type' => '1',
-                                ],
-                                [
-                                    'name' => 'Onboarding Survey B',
-                                    'label' => E::ts('Onboarding Survey B'),
-                                    'status' => 'Scheduled',
-                                    'reference_activity' => 'Onboarding call',
-                                    'reference_offset' => '1',
-                                    'reference_select' => 'newest',
-                                    'default_assignee_type' => '1',
-                                ],
-                            ],
-                        ],
-                    ],
-                    'timelineActivityTypes' => [
-                        [
-                            'name' => 'Outreach response',
-                            'label' => E::ts('Outreach response'),
-                            'status' => 'Scheduled',
-                            'reference_activity' => [],
-                            'reference_offset' => '14',
-                            'reference_select' => 'newest',
-                            'default_assignee_type' => '1',
-                        ],
-                        [
-                            'name' => 'Onboarding call',
-                            'label' => E::ts('Onboarding call'),
-                            'status' => 'Scheduled',
-                            'reference_activity' => 'Outreach response',
-                            'reference_offset' => '1',
-                            'reference_select' => 'newest',
-                            'default_assignee_type' => '1',
-                        ],
-                        [
-                            'name' => 'Onboarding Survey B',
-                            'label' => E::ts('Onboarding Survey B'),
-                            'status' => 'Scheduled',
-                            'reference_activity' => 'Onboarding call',
-                            'reference_offset' => '1',
-                            'reference_select' => 'newest',
-                            'default_assignee_type' => '1',
-                        ],
-                    ],
-                    'caseRoles' => [
-                        [
-                            'name' => 'Case Coordinator',
-                            'creator' => '1',
-                            'manager' => '1',
-                        ],
-                    ],
-                ],
+  [
+    'name' => 'CaseType_eic_investor_onboarding',
+    'entity' => 'CaseType',
+    'cleanup' => 'unused',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'name' => 'eic_investor_onboarding',
+        'title' => E::ts('Investor Onboarding'),
+        'description' => E::ts('Investor Onboarding'),
+        'definition' => [
+          'restrictActivityAsgmtToCmsUser' => 1,
+          'activityAsgmtGrps' => [],
+          'activityTypes' => [
+            [
+              'name' => 'Open Case',
+              'max_instances' => '1',
             ],
-            'match' => ['name'],
-        ],
-    ],
-    [
-        'name' => 'CaseType_eic_engagement',
-        'entity' => 'CaseType',
-        'cleanup' => 'unused',
-        'update' => 'unmodified',
-        'params' => [
-            'version' => 4,
-            'values' => [
-                'name' => 'eic_engagement',
-                'title' => E::ts('Engagement'),
-                'description' => E::ts('Engagement'),
-                'definition' => [
-                    'restrictActivityAsgmtToCmsUser' => 0,
-                    'activityAsgmtGrps' => [],
-                    'activityTypes' => [
-                        [
-                            'name' => 'Open Case',
-                            'max_instances' => '1',
-                        ],
-                        [
-                            'name' => 'Email',
-                        ],
-                        [
-                            'name' => 'Follow up',
-                        ],
-                        [
-                            'name' => 'Meeting',
-                        ],
-                        [
-                            'name' => 'Phone Call',
-                        ],
-                    ],
-                    'activitySets' => [
-                        [
-                            'name' => 'standard_timeline',
-                            'label' => E::ts('Engagement'),
-                            'timeline' => 1,
-                            'activityTypes' => [
-                                [
-                                    'name' => 'Introduction response',
-                                    'label' => E::ts('Introduction response'),
-                                    'status' => 'Scheduled',
-                                    'reference_activity' => [],
-                                    'reference_offset' => '21',
-                                    'reference_select' => 'newest',
-                                    'default_assignee_type' => '1',
-                                ],
-                            ],
-                        ],
-                    ],
-                    'timelineActivityTypes' => [
-                        [
-                            'name' => 'Introduction response',
-                            'label' => E::ts('Introduction response'),
-                            'status' => 'Scheduled',
-                            'reference_activity' => [],
-                            'reference_offset' => '21',
-                            'reference_select' => 'newest',
-                            'default_assignee_type' => '1',
-                        ],
-                    ],
-                    'caseRoles' => [
-                        [
-                            'name' => 'Case Coordinator',
-                            'creator' => '1',
-                            'manager' => '1',
-                        ],
-                    ],
-                ],
+            [
+              'name' => 'Email',
             ],
-            'match' => ['name'],
+            [
+              'name' => 'Follow up',
+            ],
+            [
+              'name' => 'Meeting',
+            ],
+            [
+              'name' => 'Phone Call',
+            ],
+            [
+              'name' => 'Task',
+            ],
+          ],
+          'activitySets' => [
+            [
+              'name' => 'standard_timeline',
+              'label' => E::ts('Outreach'),
+              'timeline' => 1,
+              'activityTypes' => [
+                [
+                  'name' => 'Follow up',
+                  'label' => E::ts('Follow up'),
+                  'status' => 'Scheduled',
+                  'reference_activity' => [],
+                  'reference_offset' => '14',
+                  'reference_select' => 'newest',
+                  'default_assignee_type' => '1',
+                  'default_subject' => 'Outreach Response',
+                ],
+              ],
+            ],
+            [
+              'name' => 'timeline_1',
+              'label' => E::ts('Onboarding'),
+              'timeline' => 1,
+              'activityTypes' => [
+                [
+                  'name' => 'Phone Call',
+                  'label' => E::ts('Phone Call'),
+                  'status' => 'Scheduled',
+                  'reference_activity' => 'Follow up',
+                  'reference_offset' => '1',
+                  'reference_select' => 'newest',
+                  'default_assignee_type' => '1',
+                  'default_subject' => 'Onboarding Call',
+                ],
+                [
+                  'name' => 'Task',
+                  'label' => E::ts('Task'),
+                  'status' => 'Scheduled',
+                  'reference_activity' => 'Phone Call',
+                  'reference_offset' => '1',
+                  'reference_select' => 'newest',
+                  'default_assignee_type' => '1',
+                  'default_subject' => 'Onboarding Survey B',
+                ],
+              ],
+            ],
+          ],
+          'timelineActivityTypes' => [
+            [
+              'name' => 'Follow up',
+              'label' => E::ts('Follow up'),
+              'status' => 'Scheduled',
+              'reference_activity' => [],
+              'reference_offset' => '14',
+              'reference_select' => 'newest',
+              'default_assignee_type' => '1',
+              'default_subject' => 'Outreach Response',
+            ],
+            [
+              'name' => 'Phone Call',
+              'label' => E::ts('Phone Call'),
+              'status' => 'Scheduled',
+              'reference_activity' => 'Follow up',
+              'reference_offset' => '1',
+              'reference_select' => 'newest',
+              'default_assignee_type' => '1',
+              'default_subject' => 'Onboarding Call',
+            ],
+            [
+              'name' => 'Task',
+              'label' => E::ts('Task'),
+              'status' => 'Scheduled',
+              'reference_activity' => 'Phone Call',
+              'reference_offset' => '1',
+              'reference_select' => 'newest',
+              'default_assignee_type' => '1',
+              'default_subject' => 'Onboarding Survey B',
+            ],
+          ],
+          'caseRoles' => [
+            [
+              'name' => 'Case Coordinator',
+              'creator' => '1',
+              'manager' => '1',
+            ],
+          ],
         ],
+      ],
+      'match' => ['name'],
     ],
+  ],
+  [
+    'name' => 'CaseType_eic_engagement',
+    'entity' => 'CaseType',
+    'cleanup' => 'unused',
+    'update' => 'unmodified',
+    'params' => [
+      'version' => 4,
+      'values' => [
+        'name' => 'eic_engagement',
+        'title' => E::ts('Engagement'),
+        'description' => E::ts('Engagement'),
+        'definition' => [
+          'restrictActivityAsgmtToCmsUser' => 0,
+          'activityAsgmtGrps' => [],
+          'activityTypes' => [
+            [
+              'name' => 'Open Case',
+              'max_instances' => '1',
+            ],
+            [
+              'name' => 'Email',
+            ],
+            [
+              'name' => 'Follow up',
+            ],
+            [
+              'name' => 'Meeting',
+            ],
+            [
+              'name' => 'Phone Call',
+            ],
+            [
+              'name' => 'Task',
+            ],
+          ],
+          'activitySets' => [
+            [
+              'name' => 'standard_timeline',
+              'label' => E::ts('Engagement'),
+              'timeline' => 1,
+              'activityTypes' => [
+                [
+                  'name' => 'Follow up',
+                  'label' => E::ts('Follow up'),
+                  'status' => 'Scheduled',
+                  'reference_activity' => 'Open Case',
+                  'reference_offset' => '21',
+                  'reference_select' => 'newest',
+                  'default_assignee_type' => '1',
+                  'default_subject' => 'Introduction Response',
+                ],
+              ],
+            ],
+          ],
+          'timelineActivityTypes' => [
+            [
+              'name' => 'Follow up',
+              'label' => E::ts('Follow up'),
+              'status' => 'Scheduled',
+              'reference_activity' => 'Open Case',
+              'reference_offset' => '21',
+              'reference_select' => 'newest',
+              'default_assignee_type' => '1',
+              'default_subject' => 'Introduction Response',
+            ],
+          ],
+          'caseRoles' => [
+            [
+              'name' => 'Case Coordinator',
+              'creator' => '1',
+              'manager' => '1',
+            ],
+          ],
+        ],
+      ],
+      'match' => ['name'],
+    ],
+  ],
 ];
