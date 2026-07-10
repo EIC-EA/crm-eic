b5603722599c5ea01d465e9a33262f8edeb7d014
Sigtechnica <siggy.loenders@sigtechnica.com>
2026-07-10 20:35:03 +0200

    feature/EICSRM-38: refactoring variables. most important settings are now variables.                    now checking civicrm mail settings are created or not then update..

--- docker/eicmailcatcher/entrypoint.sh
+++ docker/eicmailcatcher/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-mkdir -p "/var/mail/vhosts/${MAIL_DOMAIN}"
+mkdir -p "/var/mail/vhosts/${MC_MAIL_DOMAIN}"
 mkdir -p /etc/postfix/maps
 mkdir -p /var/lib/roundcube
 
@@ -13,8 +13,11 @@ envsubst < /etc/postfix/main.cf.template > /etc/postfix/main.cf
 
 count=0
 
+#domain cleanup, if they exist:
+find /var/mail/vhosts -mindepth 1 -maxdepth 1 -type d ! -name "${MC_MAIL_DOMAIN}" -exec rm -rf {} +
+  
 IFS=','
-for userpass in ${MAIL_USERS}; do
+for userpass in ${MC_MAIL_USERS}; do
   count=$((count + 1))
 
   if [ "$count" -gt 10 ]; then
@@ -32,10 +35,10 @@ for userpass in ${MAIL_USERS}; do
 
   hash="$(doveadm pw -s SHA512-CRYPT -p "$pass")"
 
-  echo "${user}@${MAIL_DOMAIN}:${hash}" >> /etc/dovecot/users
-  echo "${user}@${MAIL_DOMAIN} ${MAIL_DOMAIN}/${user}/Maildir/" >> /etc/postfix/maps/vmailbox
+  echo "${user}@${MC_MAIL_DOMAIN}:${hash}" >> /etc/dovecot/users
+  echo "${user}@${MC_MAIL_DOMAIN} ${MC_MAIL_DOMAIN}/${user}/Maildir/" >> /etc/postfix/maps/vmailbox
 
-  mkdir -p "/var/mail/vhosts/${MAIL_DOMAIN}/${user}/Maildir"
+  mkdir -p "/var/mail/vhosts/${MC_MAIL_DOMAIN}/${user}/Maildir"
 done
 ####### Single user mode!
 # cat > /etc/postfix/maps/recipient-canonical <<EOF
@@ -54,18 +57,18 @@ EOF
 
 : > /etc/postfix/maps/virtual-alias
 
-if [ "${MAIL_MODE}" = "multi" ]; then
+if [ "${MC_MAIL_MODE}" = "multi" ]; then
   IFS=','
-  for userpass in ${MAIL_USERS}; do
+  for userpass in ${MC_MAIL_USERS}; do
     user="$(echo "$userpass" | cut -d: -f1)"
     escaped_user="$(printf '%s' "$user" | sed 's/[.[\*^$()+?{}|\\]/\\&/g')"
-    escaped_domain="$(printf '%s' "$MAIL_DOMAIN" | sed 's/[.[\*^$()+?{}|\\]/\\&/g')"
+    escaped_domain="$(printf '%s' "$MC_MAIL_DOMAIN" | sed 's/[.[\*^$()+?{}|\\]/\\&/g')"
 
-    echo "/^${escaped_user}@${escaped_domain}$/ ${user}@${MAIL_DOMAIN}" >> /etc/postfix/maps/recipient-canonical
+    echo "/^${escaped_user}@${escaped_domain}$/ ${user}@${MC_MAIL_DOMAIN}" >> /etc/postfix/maps/recipient-canonical
   done
 fi
 
-echo "/^.+$/ ${CATCHALL_USER}@${MAIL_DOMAIN}" >> /etc/postfix/maps/recipient-canonical
+echo "/^.+$/ ${MC_CATCHALL_USER}@${MC_MAIL_DOMAIN}" >> /etc/postfix/maps/recipient-canonical
 ############################3
 
 
@@ -75,22 +78,22 @@ postmap /etc/postfix/maps/vmailbox
 # V1:
 # postmap /etc/postfix/maps/virtual
 
-ROUNDCUBE_DB="/var/lib/roundcube/roundcube.sqlite"
+MC_ROUNDCUBE_DB="/var/lib/roundcube/roundcube.sqlite"
 
-if [ ! -f "$ROUNDCUBE_DB" ]; then
-  sqlite3 "$ROUNDCUBE_DB" < /usr/share/dbconfig-common/data/roundcube/install/sqlite3
+if [ ! -f "$MC_ROUNDCUBE_DB" ]; then
+  sqlite3 "$MC_ROUNDCUBE_DB" < /usr/share/dbconfig-common/data/roundcube/install/sqlite3
 fi
 
-chown www-data:www-data "$ROUNDCUBE_DB"
+chown www-data:www-data "$MC_ROUNDCUBE_DB"
 
 cat > /etc/roundcube/config.inc.php <<EOF
 <?php
-\$config['db_dsnw'] = 'sqlite:///$ROUNDCUBE_DB';
+\$config['db_dsnw'] = 'sqlite:///$MC_ROUNDCUBE_DB';
 \$config['imap_host'] = 'localhost:143';
 \$config['smtp_host'] = 'localhost:25';
 \$config['smtp_user'] = '';
 \$config['smtp_pass'] = '';
-\$config['product_name'] = 'EIC Dev Mailbox';
+\$config['product_name'] = '$MC_PRODUCT_NAME';
 // change this if needed
 \$config['des_key'] = '$MC_ROUNDCUBE_KEY';
 EOF
--- lib/civicrm-ext/nc_config/install.sh
+++ lib/civicrm-ext/nc_config/install.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -veux
 
+PHP_MEMORY_LIMIT=${PHP_MEMORY_LIMIT:-1G}
+CV=/opt/drupal/vendor/bin/cv
 cv api4 Country.save --in=json < ./config/Country.json
-php -d memory_limit=$PHP_MEMORY_LIMIT -f /opt/drupal/vendor/bin/cv api4 Setting.set --in=json < ./config/components.json
-php -d memory_limit=$PHP_MEMORY_LIMIT -f /opt/drupal/vendor/bin/cv api4 Setting.set --in=json < ./config/config.json
+
+php -d memory_limit=$PHP_MEMORY_LIMIT -f $CV api4 Setting.set --in=json < ./config/components.json
+php -d memory_limit=$PHP_MEMORY_LIMIT -f $CV api4 Setting.set --in=json < ./config/config.json
 
 #mailing part:
 if ! cv api4 Extension.get +w 'key=civi_mail' +w 'status="installed"' +s key | grep -q civi_mail; then
@@ -19,12 +22,36 @@ if [ "${SES_ENABLED:-false}" = "true" ]; then
 fi
 
 envsubst < ./config/smtp.json.template > /tmp/smtp.json
-php -d memory_limit=$PHP_MEMORY_LIMIT -f /opt/drupal/vendor/bin/cv api4 Setting.set --in=json < /tmp/smtp.json
+php -d memory_limit=$PHP_MEMORY_LIMIT -f $CV api4 Setting.set --in=json < /tmp/smtp.json
 envsubst < ./config/site-email.json.template > /tmp/site-email.json
-php -d memory_limit=$PHP_MEMORY_LIMIT -f /opt/drupal/vendor/bin/cv api4 SiteEmailAddress.update --in=json < /tmp/site-email.json
-#check if exists
-envsubst < ./config/mail-account.json.template > /tmp/mail-account.json
-php -d memory_limit=$PHP_MEMORY_LIMIT -f /opt/drupal/vendor/bin/cv api4 MailSettings.create --in=json < /tmp/mail-account.json
+php -d memory_limit=$PHP_MEMORY_LIMIT -f $CV api4 SiteEmailAddress.update --in=json < /tmp/site-email.json
+
+
+
+
+#### TO DO, WHAT ON PROD
+MAIL_WHERE="name = \"$MC_IMAP_NAME\""
+MAIL_SETTINGS_ID="$(
+  php -d memory_limit="$PHP_MEMORY_LIMIT" \
+    -f "$CV" \
+    api4 MailSettings.get \
+    +w "$MAIL_WHERE" \
+    +select=id \
+    --out=json |
+  php -r '
+    $result = json_decode(stream_get_contents(STDIN), true);
+    echo $result[0]["id"] ?? "";
+  '
+)"
+
+if [ -n "$MAIL_SETTINGS_ID" ]; then
+  export MC_MAIL_SETTINGS_ID="$MAIL_SETTINGS_ID"
+  envsubst < ./config/mail-account-update.json.template > /tmp/mail-account.json
+  php -d memory_limit=$PHP_MEMORY_LIMIT -f $CV api4 MailSettings.update --in=json < /tmp/mail-account.json
+else
+  envsubst < ./config/mail-account.json.template > /tmp/mail-account.json
+  php -d memory_limit=$PHP_MEMORY_LIMIT -f $CV api4 MailSettings.create --in=json < /tmp/mail-account.json
+fi
 
 cv flush
 drush cache:rebuild
