7ab33cde3dc850931aa2de3f92319e290bd78011
Sigtechnica <siggy.loenders@sigtechnica.com>
2026-07-10 15:57:33 +0200

    feature/EICSRM-38: corrections, add ses patch to use aws cdk.

--- lib/civicrm-ext/nc_config/install.sh
+++ lib/civicrm-ext/nc_config/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -veux
 
 cv api4 Country.save --in=json < ./config/Country.json
-php -d memory_limit=$PHP_MEMORY_LIMITG -f /opt/drupal/vendor/bin/cv api4 Setting.set --in=json < ./config/components.json
-php -d memory_limit=8$PHP_MEMORY_LIMIT -f /opt/drupal/vendor/bin/cv api4 Setting.set --in=json < ./config/config.json
+php -d memory_limit=$PHP_MEMORY_LIMIT -f /opt/drupal/vendor/bin/cv api4 Setting.set --in=json < ./config/components.json
+php -d memory_limit=$PHP_MEMORY_LIMIT -f /opt/drupal/vendor/bin/cv api4 Setting.set --in=json < ./config/config.json
 
 #mailing part:
 if ! cv api4 Extension.get +w 'key=civi_mail' +w 'status="installed"' +s key | grep -q civi_mail; then
@@ -14,14 +14,15 @@ if [ "${SES_ENABLED:-false}" = "true" ]; then
   if ! cv api4 Extension.get +w 'key=ses' +w 'status="installed"' +s key | grep -q ses; then
     cv ext:enable ses
   fi
-  envsubst < ./config/ses.json.template > /tmp/ses.json
-  php -d memory_limit=$PHP_MEMORY_LIMIT -f /opt/drupal/vendor/bin/cv api4 Setting.set --in=json < /tmp/ses.json
+#  envsubst < ./config/ses.json.template > /tmp/ses.json
+#  php -d memory_limit=$PHP_MEMORY_LIMIT -f /opt/drupal/vendor/bin/cv api4 Setting.set --in=json < /tmp/ses.json
 fi
 
 envsubst < ./config/smtp.json.template > /tmp/smtp.json
 php -d memory_limit=$PHP_MEMORY_LIMIT -f /opt/drupal/vendor/bin/cv api4 Setting.set --in=json < /tmp/smtp.json
 envsubst < ./config/site-email.json.template > /tmp/site-email.json
 php -d memory_limit=$PHP_MEMORY_LIMIT -f /opt/drupal/vendor/bin/cv api4 SiteEmailAddress.update --in=json < /tmp/site-email.json
+#check if exists
 envsubst < ./config/mail-account.json.template > /tmp/mail-account.json
 php -d memory_limit=$PHP_MEMORY_LIMIT -f /opt/drupal/vendor/bin/cv api4 MailSettings.create --in=json < /tmp/mail-account.json
 
