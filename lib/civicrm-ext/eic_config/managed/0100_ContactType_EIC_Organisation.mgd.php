3a436d1bab4f09b7bcff202dd91c846fcf485f9d
Karim KANCEL <karim-andre.kancel@ext.ec.europa.eu>
2026-09-14 12:08:24 +0200

    EICSRM-260 - Rename file

--- lib/civicrm-ext/eic_config/managed/0100_ContactType_EIC_Organisation.mgd.php
+++ lib/civicrm-ext/eic_config/managed/0100_ContactType_EIC_Organisation.mgd.php
@@ -3,14 +3,14 @@ use CRM_EicConfig_ExtensionUtil as E;
 
 return [
   [
-    'name' => 'ContactType_EIC_Organization',
+    'name' => 'ContactType_EIC_Organisation',
     'entity' => 'ContactType',
     'cleanup' => 'unused',
     'update' => 'unmodified',
     'params' => [
       'version' => 4,
       'values' => [
-        'name' => 'EIC_Organization',
+        'name' => 'EIC_Organisation',
         'label' => E::ts('EIC Organisation'),
         'parent_id.name' => 'Organization',
       ],
